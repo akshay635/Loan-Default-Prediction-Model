c292f1475e86a20b62e9209a4b7a897b3d30d732
Akshay Atanure <akshayatanure11@gmail.com>
2026-01-19 19:26:30 +0530

    Update explainability.py

--- src/explainability.py
+++ src/explainability.py
@@ -16,8 +16,9 @@ class ShapExplainer:
 
     def plot(self, df):
         shap_values = self.explainer(df)
-        fig, ax = plt.subplots(figsize=(8, 6))
+        fig, ax = plt.subplots(figsize=(8, 10))
         shap.plots.waterfall(shap_values[0], max_display=10)
         return fig
+
 
 
