8725e8ac069ed8b0231a3dd54d9e566f18f3d36d
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-02 12:01:26 +0530

    Update explainability.py

--- src/explainability.py
+++ src/explainability.py
@@ -4,21 +4,27 @@ Created on Mon Jan 19 16:02:10 2026
 
 @author: aksha
 """
-
 import shap
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class ShapExplainer:
     def __init__(self, model):
-        self.explainer = shap.TreeExplainer(
-            model, feature_perturbation="tree_path_dependent"
+        self.preprocessor = model.named_steps['preprocessing']
+        self.model = model.named_steps['rf_bal']
+        self.explainer = shap.Explainer(
+            self.model, feature_perturbation="tree_path_dependent"
         )
 
     def plot(self, df):
-        shap_values = self.explainer(df)
+        df_transformed = self.preprocessor.transform(df)
+        new_df = pd.DataFrame(df_transformed, columns=self.preprocessor.get_feature_names_out())
+        shap_values = self.explainer(new_df)
         fig, ax = plt.subplots()
-        shap.plots.waterfall(shap_values[0], max_display=10)
+        shap.plots.waterfall(shap_values[0, :, 0], max_display=10)
         return fig
+
+
 
 
 
