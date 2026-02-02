29b01203650d89f5756a5ee0fcf889f7aba19a72
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-02 12:00:27 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 19 15:57:40 2026
-
-@author: aksha
-"""
-
 class RiskConfig:
     LOW_RISK = 0.30
     HIGH_RISK = 0.60
-
-    MODEL_PATH = "models/catboost_model_v1.joblib"
-
+    
+    MODEL_PATH = "C:/Users/aksha/Documents/models/loan_pred_model_v1.joblib"
+    DATA_PATH = "C:/Users/aksha/Documents/data/loan_path.csv"
+    FEATURE_IMP_PATH = "C:/Users/aksha/Documents/data/feature_imp.csv"
+    CONFUSION_MATRIX = "C:/Users/aksha/Documents/data/feature_imp.csv"
+    
     EXPECTED_COLS = [
-        'Age', 'LoanAmount', 'CreditScore', 'MonthsEmployed',
-        'NumCreditLines', 'InterestRate', 'LoanTerm', 'DTIRatio',
-        'Education', 'EmploymentType', 'MaritalStatus',
-        'HasMortgage', 'HasDependents', 'LoanPurpose',
-        'HasCoSigner', 'Monthly_Income', 'EMI'
+    'Age', 'CreditScore', 'MonthsEmployed', 'NumCreditLines', 'DTIRatio',
+       'Education', 'EmploymentType', 'MaritalStatus', 'HasMortgage',
+       'HasDependents', 'LoanPurpose', 'HasCoSigner', 'Monthly_Income', 'EMI',
+       'Post_DTI', 'age_post_dti', 'tenure_age_ratio', 'debt_stress'
     ]
+
 
