772b0c5f6b3259b9228d197597f86cc3214bb425
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-02 15:17:23 +0530

    Update loan_prediction_app.py

--- loan_prediction_app.py
+++ loan_prediction_app.py
@@ -85,7 +85,7 @@ with tab1:
             st.subheader("Personalized SHAP Explanation")
             fig, shap_values, new_columns = explainer.plot(df)
             st.pyplot(fig, use_container_width=False)
-            generate_shap_insight(df,shap_values,top_n = 5)
+            generate_shap_insight(new_columns, shap_values, top_n = 5)
             st.caption(
             """Features pushing the risk higher are shown in red, 
                while features reducing risk are shown in blue.""")
@@ -125,6 +125,7 @@ with tab2:
     )
 
 st.caption("This dashboard provides readiness estimation only. Final lending decisions must follow business policies.")
+
 
 
 
