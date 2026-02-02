66267bb07c3362dafb1bf5067b549343889dc4de
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-02 12:25:46 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,10 +3,10 @@ class RiskConfig:
     LOW_RISK = 0.30
     HIGH_RISK = 0.60
     
-    MODEL_PATH = "C:/Users/aksha/Documents/models/loan_pred_model_v1.joblib"
-    DATA_PATH = "C:/Users/aksha/Documents/data/loan_path.csv"
-    FEATURE_IMP_PATH = "C:/Users/aksha/Documents/data/feature_imp.csv"
-    CONFUSION_MATRIX = "C:/Users/aksha/Documents/data/feature_imp.csv"
+    MODEL_PATH = "models/loan_pred_model_v1.joblib"
+    DATA_PATH = "data/Loan_default.csv"
+    FEATURE_IMP_PATH = "data/feature_imp.csv"
+    CONFUSION_MATRIX = "data/feature_imp.csv"
     
     EXPECTED_COLS = [
     'Age', 'CreditScore', 'MonthsEmployed', 'NumCreditLines', 'DTIRatio',
@@ -14,5 +14,6 @@ class RiskConfig:
        'HasDependents', 'LoanPurpose', 'HasCoSigner', 'Monthly_Income', 'EMI',
        'Post_DTI', 'age_post_dti', 'tenure_age_ratio', 'debt_stress'
     ]
+
 
 
