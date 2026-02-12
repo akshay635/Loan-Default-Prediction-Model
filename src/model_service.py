a924a4c256f00a1f93d559a2fe71f0c60e4eb5bc
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-12 15:39:29 +0530

    Update model_service.py

--- src/model_service.py
+++ src/model_service.py
@@ -8,13 +8,14 @@ Created on Mon Jan 19 15:59:52 2026
 import joblib
 import streamlit as st
 
+@st.cache_resource
 class LoanRiskModel:
     def __init__(self, model_path):
         self.model = self._load_model(model_path)
 
-    @st.cache_resource
     def _load_model(_self, path):
         return joblib.load(path)
 
     def predict_proba(self, df):
         return self.model.predict_proba(df)[0, 1]
+
