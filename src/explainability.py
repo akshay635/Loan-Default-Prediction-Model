1ec919784d95457e0d732ee443d034c7b51d4d4e
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-12 13:07:44 +0530

    Update explainability.py

--- src/explainability.py
+++ src/explainability.py
@@ -18,20 +18,10 @@ class ShapExplainer:
         df_transformed = self.preprocessor.transform(df)
         new_df = pd.DataFrame(df_transformed, columns=self.preprocessor.get_feature_names_out())
         shap_values = self.explainer(new_df)
-        friendly_names = [name.split("_")[1] for name in self.preprocessor.get_feature_names_out()]
-        shap_row = shap_values[0, :, 1]  # one sample, one class
-
-        groups = defaultdict(list)
-        for i, name in enumerate(friendly_names):
-            groups[name].append(i)
-        
-        merged_shap = {base: np.sum(shap_row[idxs]) for base, idxs in groups.items()}
-        values = np.array(list(merged_shap.values()))
-        names = list(merged_shap.keys())
-        merged_explanation = shap.Explanation(values=values, feature_names=names)
         fig, ax = plt.subplots()
-        shap.plots.waterfall(merged_explanation[0,1])
+        shap.plots.waterfall(shap_values[0, :, 1])
         return fig
+
 
 
 
