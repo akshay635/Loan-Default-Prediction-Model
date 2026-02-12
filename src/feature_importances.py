dfe47495d9b7bc759f9b2c8ea54c4b5e63238906
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-12 12:20:15 +0530

    Create feature_importances.py

---
+++ src/feature_importances.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import plotly as px
+
+# Feature Importances
+def Feature_IMP(df):
+  fig = px.bar(
+              df.head(10),
+              x="Importances",
+              y="Features",
+              title="Top Features Driving Model Decisions",
+              text_auto=True)
+  
+  fig.update_layout(yaxis=dict(autorange="reversed"))
+
+  return fig
