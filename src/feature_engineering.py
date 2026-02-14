ef0356094b69c46c651243d52de04787cf53defd
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-14 18:00:43 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,12 +1,13 @@
 # imporrting Pandas
 import pandas as pd
 
-def FE(df):
-  # Feature engineering
-  df['EMI/Income_ratio'] = round((df['EMI'] / df['Monthly_Income']), 2)
-  df['Post_DTI'] = df['DTIRatio'] + df['EMI/Income_ratio']
-  df['age_post_dti'] = df['Age'] * df['Post_DTI']
-  df['tenure_age_ratio'] = df['MonthsEmployed'] / (df['Age'] + 1e-6)
-  df['debt_stress'] = df['EMI/Income_ratio'] * df['DTIRatio']
+class Feature_Engineering:
+  def derived_features(df):
+    # Feature engineering
+    df['EMI/Income_ratio'] = round((df['EMI'] / df['Monthly_Income']), 2)
+    df['Post_DTI'] = df['DTIRatio'] + df['EMI/Income_ratio']
+    df['age_post_dti'] = df['Age'] * df['Post_DTI']
+    df['tenure_age_ratio'] = df['MonthsEmployed'] / (df['Age'] + 1e-6)
+    df['debt_stress'] = df['EMI/Income_ratio'] * df['DTIRatio']
 
-  return df
+    return df
