08a059872569b6bd9bfc698e655a1b859f672226
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-14 18:03:38 +0530

    Refactor feature engineering methods and add visualization
    Refactor derived_features method and add Feature_IMP method for visualizing feature importances.

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,9 +1,10 @@
 # imporrting Pandas
 import pandas as pd
+import plotly.express as px
 
 class Feature_Engineering:
-  def derived_features(df):
-    # Feature engineering
+  # Feature engineering
+  def derived_features(self, df):
     df['EMI/Income_ratio'] = round((df['EMI'] / df['Monthly_Income']), 2)
     df['Post_DTI'] = df['DTIRatio'] + df['EMI/Income_ratio']
     df['age_post_dti'] = df['Age'] * df['Post_DTI']
@@ -11,3 +12,15 @@ class Feature_Engineering:
     df['debt_stress'] = df['EMI/Income_ratio'] * df['DTIRatio']
 
     return df
+  # Feature Importances
+  def Feature_IMP(self, df):
+    fig = px.bar(
+                df.head(10),
+                x="Importances",
+                y="Features",
+                title="Top Features Driving Model Decisions",
+                text_auto=True)
+    
+    fig.update_layout(yaxis=dict(autorange="reversed"))
+  
+    return fig
