e71763bd0d5c07113454ce61efdb95bfbc9d8bfb
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-18 18:23:17 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,19 +3,20 @@ class RiskConfig:
     LOW_RISK = 0.35
     HIGH_RISK = 0.60
     
-    MODEL_PATH = "models/loan_pred_model_v1.joblib"
+    MODEL_PATH = "models/loan_pred_model_v2.joblib"
     DATA_PATH = "data/Loan_default.csv"
-    FEATURE_IMP_PATH = "data/feature_imp.csv"
+    FEATURE_IMP_PATH = "data/forest_imp.csv"
     CONFUSION_MATRIX = "data/confusion_matrix.csv"
     
     EXPECTED_COLS = [ 
        'Age', 'CreditScore', 'MonthsEmployed', 'NumCreditLines', 'DTIRatio',
        'Education', 'EmploymentType', 'MaritalStatus', 'HasMortgage',
-       'HasDependents', 'LoanPurpose', 'HasCoSigner', 'Monthly_Income', 'EMI',
+       'HasDependents', 'LoanPurpose', 'HasCoSigner', 'MonthlyIncome', 'EMI',
        'Post_DTI', 'age_post_dti', 'tenure_age_ratio', 'debt_stress'
     ]
 
     TARGET_COL = ['Default']
+
 
 
 
