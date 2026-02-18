468155bb6fd449076aec08b51dba3f04da9550eb
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-18 22:24:09 +0530

    Update model_service.py

--- src/model_service.py
+++ src/model_service.py
@@ -3,17 +3,22 @@
 import joblib
 import streamlit as st
 
+@st.cache_resource
+def load_model(path):
+    return joblib.load(path)
+
 class LoanRiskModel:
     def __init__(self, model_path):
         self.model = self.load_model(model_path)
         
-    @st.cache_resource
-    def load_model(self, path):
-        return joblib.load(path)
-        
     @st.cache_data
     def predict_proba(self, df):
-        return self.model.predict_proba(df)[0, 1]
+        return self.model.predict_proba(df)
+        if proba.ndim == 2:
+            return proba[:, 1]   # Return all class-1 probabilities
+        else:
+            return proba
+
 
 
 
