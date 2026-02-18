8cfcc555d6a3bd4d6fca4115aaf07666331cda05
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-18 22:45:36 +0530

    Update model_service.py

--- src/model_service.py
+++ src/model_service.py
@@ -9,15 +9,16 @@ def load_model(path):
 
 class LoanRiskModel:
     def __init__(self, model_path):
-        self.model = self.load_model(model_path)
+        self.model = load_model(model_path)
         
     @st.cache_data
     def predict_proba(self, df):
-        return self.model.predict_proba(df)
+        proba = self.model.predict_proba(df)
         if proba.ndim == 2:
             return proba[:, 1]   # Return all class-1 probabilities
         else:
             return proba
+
 
 
 
