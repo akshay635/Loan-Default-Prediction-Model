c463f5fdb32f8c68f120fb07764cdea39cfafce1
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-25 23:15:46 +0530

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,15 +3,6 @@ import pandas as pd
 import plotly.express as px
 
 class FeatureEngineering:
-  # Feature engineering
-  def derived_features(self, df):
-    df['EMI/Income_ratio'] = round((df["EMI"] / (df["MonthlyIncome"] + 1e-6)), 2)
-    df['Post_DTI'] = df['DTIRatio'] + df['EMI/Income_ratio']
-    df['age_post_dti'] = df['Age'] * df['Post_DTI']
-    df['tenure_age_ratio'] = df['MonthsEmployed'] / (df['Age'] + 1e-6)
-    df['debt_stress'] = df['EMI/Income_ratio'] * df['DTIRatio']
-    return df
-  
   # Feature Importances
   def Feature_IMP(self, df):
     fig = px.bar(
