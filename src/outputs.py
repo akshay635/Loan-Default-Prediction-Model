3db752b12afd64fb3f671cd7eb361bc4c0596554
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-28 11:00:01 +0530

    Update outputs.py

--- src/outputs.py
+++ src/outputs.py
@@ -22,7 +22,7 @@ class RiskAssessment:
         prob = self.model.predict_proba(df)
         risk, action = self.decision_engine.decide(prob)
         expected_loss = prob*(df.iloc[0]['LoanAmount'])*0.7
-        el_percent = prob * 0.7
+        el_percent = (prob*0.7)*100
         col1, col2 = st.columns([1, 1])
         with col1:
             # Narrative output
@@ -46,7 +46,7 @@ class RiskAssessment:
     
             st.markdown(f"**Suggested Action:** {action}")
             st.metric("Expected Loss", f"₹ {expected_loss:.2f}")
-            st.metric(f"Expected Loss %: {el_percent * 100:.2f}%")
+            st.metric(f"Expected Loss %: {el_percent:.2f}%")
             
         with col2:
             fig = go.Figure(go.Indicator(
