ba9b85d0854670597c1f88052030b6f3d658ff21
Akshay Atanure <akshayatanure11@gmail.com>
2026-02-28 11:38:13 +0530

    Update loan_prediction_app.py

--- loan_prediction_app.py
+++ loan_prediction_app.py
@@ -74,6 +74,9 @@ with tab2:
 
         df, y_proba, y_pred, y_true, threshold = batch_data_modeling(df)
         tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+
+        expected_loss = df['Expected_loss'].mean()
+        el_percent = df['EL_percent(%)'].mean()
         
         recall = tp / (tp + fn)
         miss_rate = fn / (tp + fn)
@@ -84,12 +87,13 @@ with tab2:
 
         st.header("📌 Portfolio Summary")
 
-        col1, col2, col3, col4 = st.columns(4)
-        
-        col1.metric("Total Records", len(df))
-        col2.metric("Flagged High Risk", f"{flagged_rate*100:.2f}%")
-        col3.metric("Recall (Catch Rate)", f"{recall*100:.2f}%")
-        col4.metric("Miss Rate", f"{miss_rate*100:.2f}%")
+        col1, col2, col3, col4, col5, col6 = st.columns(6)
+        col1.metric("Expected Loss", f"₹ {expected_loss:.2f}")
+        col2.metric("Expected Loss %:", f"{el_percent:.2f}%")
+        col3.metric("Total Records", len(df))
+        col4.metric("Flagged High Risk", f"{flagged_rate*100:.2f}%")
+        col5.metric("Recall (Catch Rate)", f"{recall*100:.2f}%")
+        col6.metric("Miss Rate", f"{miss_rate*100:.2f}%")
 
         st.subheader("🔎 Confusion Matrix")
 
@@ -148,6 +152,7 @@ with tab5:
 
     # To display gauge in Streamlit:
     st.plotly_chart(calc.plot_gauge())
+
 
 
 
