faa30e433a0ad775d930745c8d18dcb7261edb72
Akshay Atanure <akshayatanure11@gmail.com>
2026-03-10 18:23:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 from training.train import main
 
-import mlflow
-import os
-
-# Ensure directories exist
-os.makedirs("outputs/mlflow/mlruns", exist_ok=True)
-
-# Point MLflow to SQLite backend
-mlflow.set_tracking_uri("sqlite:///outputs/mlflow/mlflow.db")
-
-# Optional: set experiment name
-mlflow.set_experiment("default")
-
 if __name__ == "__main__":
     main()
