eb6158bb17231db9d13945a700874110d4c612ba
Akshay Atanure <akshayatanure11@gmail.com>
2026-03-12 00:50:59 +0530

    Remove MLflow tracking configuration
    Removed MLflow tracking setup from main.py.

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from training.train import main
-import mlflow
-import os
-
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_experiment("Loan_Default_Project")
 
 if __name__ == "__main__":
     main()
